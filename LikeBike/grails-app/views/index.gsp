3cf9651178ecb99f54ca9d4a7551542b6faa318c
igor <igor@igor-R425D-R525D.(none)>
2013-08-02 02:32:59 +0700

    draw heatmap

--- LikeBike/grails-app/views/index.gsp
+++ LikeBike/grails-app/views/index.gsp
@@ -3,11 +3,40 @@
   <head>
     <title>Примеры. Построение маршрута</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <script src="//api-maps.yandex.ru/2.0/?load=package.standard,package.geoObjects,package.route&lang=ru-RU" type="text/javascript"></script>
+    <script src="//api-maps.yandex.ru/2.0/?load=package.standard,package.geoObjects&lang=ru-RU" type="text/javascript"></script>
     <script src="http://yandex.st/jquery/1.6.4/jquery.min.js" type="text/javascript"></script>
     <g:javascript src="mapload.js" />
+    <g:javascript src="heatMap/heatmap.js" />
+    <!------------------------------------------------------------------------>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+      
+      #heatmapArea {
+        position:relative;
+        float:left;
+        width:600px;
+        height:400px;
+        background-image:url(http://www.patrick-wied.at/static/heatmapjs/demo/realtime_heatmap/graphic.png);
+        border:1px dashed black;
+      }
+      #canvasMap{
+        width: 800px;
+        height: 600px;
+      }
+    </style>
+    <g:javascript src="/heatMap/heatmap.js" />
+    <link rel="shortcut icon" type="image/png" href="http://www.patrick-wied.at/img/favicon.png" />
+
   </head>
   <body>
     <div id="map" style="width:800px; height:600px"></div>
+    <!-------------------------------------------------------------------------->
+
+    
+    
+    <script type="text/javascript">
+    
+    </script>
+
   </body>
 </html>
--- LikeBike/web-app/js/mapload.js
+++ LikeBike/web-app/js/mapload.js
@@ -1,32 +1,77 @@
-const  MIN_LAT = 54;
-const  MAX_LAT = 55;
-const  MIN_LONG = 74;
-const  MAX_LONG = 76;
-
-
 ymaps.ready(init);
 
 function init() {
     var myMap = new ymaps.Map("map", {
-        center: [54.94835, 73.364996],
+        center: [55.04835, 73.364996],
         zoom: 12
     });
     var myPlacemark = new ymaps.Placemark([54.94835, 73.364996], {
         content: 'Омск!',
         balloonContent: 'Это наш город!!!'
     });
-    for (var i = 0; i < 100; i++) {
-      ymaps.route([
-        [Math.random() * (MAX_LAT - MIN_LAT) + MIN_LAT, Math.random() * (MAX_LONG - MIN_LONG) + MIN_LONG],
-        [Math.random() * (MAX_LAT - MIN_LAT) + MIN_LAT, Math.random() * (MAX_LONG - MIN_LONG) + MIN_LONG]
-      ]).then(function (route) {
-        myMap.geoObjects.add(route);
-      })
-    }
 
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    //window.onload = function(){
+      //alert('hello');
+      //alert(myMap.converter.clientPixelsToCoordinates(new YMaps.point(200, 300)));
+      
+
+
+      var xx = h337.create({"element":document.getElementById("map"), "radius": 10, "visible":true});
+      //var pixelsCoords = myMap.coordSystem.fromCoordPoint(myMap.getCenter());
+      var mainCanvas = xx.get('canvas');
+      //mainCanvas.getContext('2d').fillRect(0,0,mainCanvas.width,mainCanvas.height);
+    //};
+    mainCanvas.width = 800;
+    mainCanvas.height = 600;
+    mainCanvas.style.left = "-400px";
+    mainCanvas.style.top = "-300px";
+    mainCanvas.style.position = "absolute";
+
+    
+
+      var projection = myMap.options.get('projection');
+    //xx.store.addDataPoint(pixelsCoords.x, pixelsCoords.y);
+
+
+    /*document.getElementById("map").onclick = function(ev){
+        var pos = h337.util.mousePosition(ev);
+        //console.log(pos[0]);
+        //xx.store.addDataPoint(pos[0], pos[1]);
+    };*/
+    var pointsArray = [];
+    //document.getElementById("map").onclick
+    myMap.events.add('boundschange', function(ev){
+        mainCanvas.getContext('2d').clearRect(0, 0, mainCanvas.width, mainCanvas.height);
+        xx.clear();
+        //alert(point[0] + " " + point[1]);
+        //mainCanvas.getContext('2d').fillRect(0,0,mainCanvas.width,mainCanvas.height);
+        pointsArray.forEach(function(point){
+            var p = myMap.converter.globalToPage(
+            projection.toGlobalPixels(
+              // географические координаты
+              [point[0], point[1]],
+              myMap.getZoom()
+            )
+            );
+            xx.store.addDataPoint(p[0], p[1]);
+        });
+        //xx.store.addDataPoint(point[0], point[1]);
+
+    });
+
+    //var managerLayers = myMap.panes.get('layers');
+    //managerLayers.add(xx.get("canvas"));
+    myMap.panes.get('layers').getElement().appendChild(xx.get('canvas'));
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+    
     $.ajax({
       url: "route/get_route", 
-      type: "post", 
+      type: "get", 
       dataType: "json",
     
       success: function(data){
@@ -38,6 +83,17 @@ function init() {
               Point.push(val_c);
             })
             myRoute.push(Point);
+            pointsArray.push(Point);
+            ////////////////////////////////////////////////////////
+            var point = myMap.converter.globalToPage(
+            projection.toGlobalPixels(
+              // географические координаты
+              [Point[0], Point[1]],
+              myMap.getZoom()
+            )
+            );
+            xx.store.addDataPoint(point[0], point[1]);
+            ///////////////////////////////////////////////////////
           })
           var myGeoObject = new ymaps.GeoObject({
             geometry: {  
@@ -64,7 +120,9 @@ function init() {
           alert(data);
       }
     })
+
     myMap.controls.add('zoomControl');
     myMap.controls.add('typeSelector');
+    //xx.store.addDataPoint(54.94835, 73.364996);
     //myMap.geoObjects.add(myPlacemark);
 }
