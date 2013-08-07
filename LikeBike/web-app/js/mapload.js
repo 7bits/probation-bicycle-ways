2c67068512a8bc5b1a459321ec6b045d30fc7d13
igorperzh <igorperzh@gmail.com>
2013-08-07 18:29:51 +0700

    google map

--- LikeBike/web-app/js/mapload.js
+++ LikeBike/web-app/js/mapload.js
@@ -1,75 +1,18 @@
-ymaps.ready(init);
+var map, pointarray, heatmap;
 
-function init() {
-    var myMap = new ymaps.Map("map", {
-        center: [55.04835, 73.364996],
-        zoom: 12
-    });
-    var myPlacemark = new ymaps.Placemark([54.94835, 73.364996], {
-        content: 'Омск!',
-        balloonContent: 'Это наш город!!!'
-    });
+var taxiData = [];
 
-    ///////////////////////////////////////////////////////////////////////////
-    ///////////////////////////////////////////////////////////////////////////
+function initialize() {
+  var mapOptions = {
+    zoom: 12,
+    center: new google.maps.LatLng(54.988744, 73.369271),
+    mapTypeId: google.maps.MapTypeId.SATELLITE
+  };
 
-    //window.onload = function(){
-      //alert('hello');
-      //alert(myMap.converter.clientPixelsToCoordinates(new YMaps.point(200, 300)));
-      
-
-
-      var xx = h337.create({"element":document.getElementById("map"), "radius": 10, "visible":true});
-      //var pixelsCoords = myMap.coordSystem.fromCoordPoint(myMap.getCenter());
-      var mainCanvas = xx.get('canvas');
-      //mainCanvas.getContext('2d').fillRect(0,0,mainCanvas.width,mainCanvas.height);
-    //};
-    mainCanvas.width = 800;
-    mainCanvas.height = 600;
-    mainCanvas.style.left = "-400px";
-    mainCanvas.style.top = "-300px";
-    mainCanvas.style.position = "absolute";
-
-    
-
-      var projection = myMap.options.get('projection');
-    //xx.store.addDataPoint(pixelsCoords.x, pixelsCoords.y);
-
-
-    /*document.getElementById("map").onclick = function(ev){
-        var pos = h337.util.mousePosition(ev);
-        //console.log(pos[0]);
-        //xx.store.addDataPoint(pos[0], pos[1]);
-    };*/
-    var pointsArray = [];
-    //document.getElementById("map").onclick
-    myMap.events.add('boundschange', function(ev){
-        mainCanvas.getContext('2d').clearRect(0, 0, mainCanvas.width, mainCanvas.height);
-        xx.clear();
-        //alert(point[0] + " " + point[1]);
-        //mainCanvas.getContext('2d').fillRect(0,0,mainCanvas.width,mainCanvas.height);
-        pointsArray.forEach(function(point){
-            var p = myMap.converter.globalToPage(
-            projection.toGlobalPixels(
-              // географические координаты
-              [point[0], point[1]],
-              myMap.getZoom()
-            )
-            );
-            xx.store.addDataPoint(p[0], p[1]);
-        });
-        //xx.store.addDataPoint(point[0], point[1]);
-
-    });
-
-    //var managerLayers = myMap.panes.get('layers');
-    //managerLayers.add(xx.get("canvas"));
-    myMap.panes.get('layers').getElement().appendChild(xx.get('canvas'));
-
-    ///////////////////////////////////////////////////////////////////////////
-    ///////////////////////////////////////////////////////////////////////////
-    
-    $.ajax({
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+  console.log("start adding points");
+  $.ajax({
       url: "route/get_route", 
       type: "get", 
       dataType: "json",
@@ -82,47 +25,72 @@ function init() {
             $.each(val_p, function(coord, val_c) {
               Point.push(val_c);
             })
-            myRoute.push(Point);
-            pointsArray.push(Point);
-            ////////////////////////////////////////////////////////
-            var point = myMap.converter.globalToPage(
-            projection.toGlobalPixels(
-              // географические координаты
-              [Point[0], Point[1]],
-              myMap.getZoom()
-            )
-            );
-            xx.store.addDataPoint(point[0], point[1]);
-            ///////////////////////////////////////////////////////
+            taxiData.push(new google.maps.LatLng(Point[0], Point[1]));
+            //console.log(Point[0] + " " + Point[1]);
           })
-          var myGeoObject = new ymaps.GeoObject({
-            geometry: {  
-                type: "LineString",
-                coordinates: myRoute
-            },
-            properties:{
-                hintContent: "маршрут",
-                balloonContent: "тут кто-то проехал"
-            }
-          },{
-            draggable: false,
-            strokeColor: "#FF0000",
-            strokeWidth: 5,
-            strokeOpacity: 0.5
-          });
-
-    myMap.geoObjects.add(myGeoObject);  
         });
+        var pointArray = new google.maps.MVCArray(taxiData);
+
+        heatmap = new google.maps.visualization.HeatmapLayer({
+          data: pointArray, radius: 8, opacity: 0.5/*, gradient: [
+    'rgba(0, 255, 255, 0)',
+    'rgba(255, 0, 51, 1)',
+    'rgba(0, 204, 255, 1)',
+    /*'rgba(0, 204, 255, 1)',
+    'rgba(0, 204, 255, 1)',
+    'rgba(0, 204, 255, 1)',
+    'rgba(0, 204, 255, 1)',
+    'rgba(0, 204, 255, 1)',
+    'rgba(0, 204, 255, 1)',
+    'rgba(0, 0, 127, 1)',
+    'rgba(63, 0, 91, 1)',
+    'rgba(127, 0, 63, 1)',
+    'rgba(191, 0, 31, 1)',
+    'rgba(255, 102, 0, 1)'
+  ]*/
+        });
+        heatmap.setMap(map);
       },
     
       error: function(jqXHR){
           data = jQuery.parseJSON( jqXHR.responseText );
           alert(data);
       }
-    })
+  })//end ajax
 
-    myMap.controls.add('zoomControl');
-    myMap.controls.add('typeSelector');
-    //xx.store.addDataPoint(54.94835, 73.364996);
-    //myMap.geoObjects.add(myPlacemark);
+  console.log("end adding points");
+}
+
+function toggleHeatmap() {
+  heatmap.setMap(heatmap.getMap() ? null : map);
+}
+
+function changeGradient() {
+  var gradient = [
+    'rgba(0, 255, 255, 0)',
+    'rgba(0, 255, 255, 1)',
+    'rgba(0, 191, 255, 1)',
+    'rgba(0, 127, 255, 1)',
+    'rgba(0, 63, 255, 1)',
+    'rgba(0, 0, 255, 1)',
+    'rgba(0, 0, 223, 1)',
+    'rgba(0, 0, 191, 1)',
+    'rgba(0, 0, 159, 1)',
+    'rgba(0, 0, 127, 1)',
+    'rgba(63, 0, 91, 1)',
+    'rgba(127, 0, 63, 1)',
+    'rgba(191, 0, 31, 1)',
+    'rgba(255, 102, 0, 1)'
+  ]
+  heatmap.setOptions({
+    gradient: heatmap.get('gradient') ? null : gradient
+  });
+}
+
+function changeRadius() {
+  heatmap.setOptions({radius: heatmap.get('radius') ? null : 20});
+}
+
+function changeOpacity() {
+  heatmap.setOptions({opacity: heatmap.get('opacity') ? null : 0.2});
 }
