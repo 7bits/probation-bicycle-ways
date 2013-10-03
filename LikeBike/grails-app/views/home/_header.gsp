a0279667d83ec7cc6aa9b2d174ab4cc57ecb443f
sevenbits <sevenbits@sevenbits-H55M-D2H.(none)>
2013-10-03 15:32:53 +0700

    header have Adaptable page layout

--- LikeBike/grails-app/views/home/_header.gsp
+++ LikeBike/grails-app/views/home/_header.gsp
@@ -7,13 +7,16 @@
 </head>
 
 <body>
-
+<div id="before_menu"></div>
 <header>
-    <g:img id="razmetka" dir="images" file="razmetka.png"/>
-    <g:img id="logo" dir="images" file="logo.png"/>
-    <p id="label_download_app">Скачать приложение:</p>
-    <a href="#"><g:img id="button_android_link" dir="images" file="android.png"/></a>
-    <a href="#"><g:img id="button_app_store_link" dir="images" file="appStore.png"/></a>
+    <div id="on_header">
+        <div id="logo"><g:img dir="images" file="logo.png"/></div>
+        <div id="download_app">
+            <p id="label_download_app">Скачать приложение:</p>
+            <a href="#"><g:img id="button_android_link" dir="images" file="android.png"/></a>
+            <a href="#"><g:img id="button_app_store_link" dir="images" file="appStore.png"/></a>
+        </div>
+    </div>
 </header>
 
 <menu>
@@ -25,19 +28,21 @@
         <li class="menu_list_item">
             <p class="menu_text"><a class="menu_a" href="map">КАРТА МАРШРУТОВ</a></p>
         </li>
-        <li class="menu_list_item">
-            <p style="cursor: pointer;" id="p_reg" class="menu_text">РЕГИСТРАЦИЯ</p>
-        </li>
-
-        <li class="menu_list_item">
-            <sec:ifLoggedIn>
+        <sec:ifLoggedIn>
+            <li class="menu_list_item">
                 <p style="cursor: pointer;" id="p_log" class="menu_text">Вы зашли как <sec:username/> (<g:link
                         controller='logout'>Выйти</g:link>)</p>
-            </sec:ifLoggedIn>
-            <sec:ifNotLoggedIn>
+            </li>
+        </sec:ifLoggedIn>
+        <sec:ifNotLoggedIn>
+            <li class="menu_list_item">
+                <p style="cursor: pointer;" id="p_reg" class="menu_text">РЕГИСТРАЦИЯ</p>
+            </li>
+            <li class="menu_list_item">
                 <p style="cursor: pointer;" id="p_log" class="menu_text">ВХОД</p>
-            </sec:ifNotLoggedIn>
-        </li>
+            </li>
+        </sec:ifNotLoggedIn>
     </ul>
 </menu>
+<span id="after"></span>
 </body>
--- LikeBike/web-app/js/registration.js
+++ LikeBike/web-app/js/registration.js
@@ -5,13 +5,15 @@ $('document').ready(function () {
         //document.getElementById('reg').style.hight = '660px';
         document.getElementById('parent_reg').style.display = 'none';
     }
-    document.getElementById('p_reg').onclick = function () {
-        document.getElementById('p_ok').style.display = 'none';
-        document.getElementById('parent_reg').style.display = 'block';
-        document.getElementById('register_form').style.display = 'block';
+    if (document.getElementById('p_reg') != null) {
+        document.getElementById('p_reg').onclick = function () {
+            document.getElementById('p_ok').style.display = 'none';
+            document.getElementById('parent_reg').style.display = 'block';
+            document.getElementById('register_form').style.display = 'block';
+        }
     }
 
-    $('#register_form').submit(function(e){
+    $('#register_form').submit(function (e) {
         e.preventDefault();
         var command = {
             username: $('#username').val(),
@@ -23,49 +25,49 @@ $('document').ready(function () {
             command: command
         }
         $.ajax({
-            type : 'POST',
-            url : 'register/register',
+            type: 'POST',
+            url: 'register/register',
             datatype: JSON,
-            data : command,
-            success: function(data){
-                if(data.hasError) {
+            data: command,
+            success: function (data) {
+                if (data.hasError) {
                     document.getElementById('password').value = '';
                     document.getElementById('password2').value = '';
 
-                    if(data.username == 'right') {
+                    if (data.username == 'right') {
                         document.getElementById('username_error').innerHTML = '';
                     }
-                    if(data.email == 'right') {
+                    if (data.email == 'right') {
                         document.getElementById('email_error').innerHTML = '';
                     }
-                    if(data.password == 'right') {
+                    if (data.password == 'right') {
                         document.getElementById('password_error').innerHTML = '';
                     }
-                    if(data.password2 == 'right') {
+                    if (data.password2 == 'right') {
                         document.getElementById('password2_error').innerHTML = '';
                     }
 
 
-                    if(data.username == 'userNameHold') {
+                    if (data.username == 'userNameHold') {
                         document.getElementById('username_error').innerHTML = 'Данное имя уже ипользуется.';
                     }
-                    if(data.username == 'empty') {
+                    if (data.username == 'empty') {
                         document.getElementById('username_error').innerHTML = 'Имя не должно быть пустым';
                     }
-                    if(data.email == 'empty') {
+                    if (data.email == 'empty') {
                         document.getElementById('email_error').innerHTML = 'email не должен быть пустым';
                     }
-                    if(data.email == 'emailNotValide') {
+                    if (data.email == 'emailNotValide') {
                         document.getElementById('email_error').innerHTML = 'Неверный email.';
                     }
-                    if(data.password != 'right') {
+                    if (data.password != 'right') {
                         document.getElementById('password_error').innerHTML = 'Неверный пароль. Пароль не должен быть короче 8 символов.';
                     }
-                    if(data.password2 == 'passwordNotMatch') {
+                    if (data.password2 == 'passwordNotMatch') {
                         document.getElementById('password2_error').innerHTML = 'Пароли не совпадают.';
                     }
                 }
-                if(!data.hasError) {
+                if (!data.hasError) {
                     document.getElementById('username').value = '';
                     document.getElementById('email').value = '';
                     document.getElementById('password').value = '';
@@ -75,8 +77,8 @@ $('document').ready(function () {
                 }
 
             },
-            error: function(jqXHR){
-                data = jQuery.parseJSON( jqXHR.responseText );
+            error: function (jqXHR) {
+                data = jQuery.parseJSON(jqXHR.responseText);
                 alert("ERROR");
             }
         });
