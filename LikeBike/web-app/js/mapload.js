8c4f2459ec70a3af7a9a871e78cbd0407a51f81a
Filter94 <IamFilter94@gmail.com>
2014-07-24 14:08:45 +0700

    Made solid route view

--- LikeBike/web-app/js/mapload.js
+++ LikeBike/web-app/js/mapload.js
@@ -6,14 +6,6 @@ var line = [];//список линий на карте
 var heatmap = 0;//список точек
 
 $("document").ready(function () {
-
-
-    document.getElementById('screen').onclick = function () {
-        var routeCount = 2;
-        var urlImg = routeToStaticMapURL(routeArray[routeCount]);
-        document.getElementById('route_img').src = urlImg;
-
-    }
     $("#userfile").change(function () {
         document.getElementById("import_p").innerHTML = document.getElementById('userfile').value;
     });
@@ -132,6 +124,36 @@ $("document").ready(function () {
         }
     ]);
 
+    $.ajax({
+        type: "GET",
+        url: "ways/Afternoon Ride.gpx",
+        dataType: "xml",
+        success: function(xml) {
+            var points = [];
+            var bounds = new google.maps.LatLngBounds ();
+            $(xml).find("trkpt").each(function() {
+                var lat = $(this).attr("lat");
+                var lon = $(this).attr("lon");
+                var p = new google.maps.LatLng(lat, lon);
+                points.push(p);
+                bounds.extend(p);
+            });
+
+            var poly = new google.maps.Polyline({
+                // use your own style here
+                path: points,
+                strokeColor: "#0000FF",
+                strokeOpacity: .7,
+                strokeWeight: 3
+            });
+
+            poly.setMap(map);
+
+            // fit bounds to track
+            map.fitBounds(bounds);
+        }
+    });
+
     var loader = document.createElement("div");
     loader.style.width = "126px";
     loader.style.height = "22px";
@@ -152,7 +174,7 @@ $("document").ready(function () {
             routeMode = 0;
         }
         displayRoute();
-    }
+    };
     map.controls[google.maps.ControlPosition.LEFT].push(mode);
 
     $.ajax({
@@ -172,10 +194,20 @@ $("document").ready(function () {
                     myRoute.push(googleMapPoint);
                 });
                 routeArray.push(myRoute);
+                var poly = new google.maps.Polyline({
+                    // use your own style here
+                    path: myRoute,
+                    strokeColor: "#0000FF",
+                    strokeOpacity: .7,
+                    strokeWeight: 3
+                });
+
+                poly.setMap(map);
+
+                // fit bounds to track
+                map.fitBounds(bounds);
             });
-            createRoute();
-            displayRoute();
-            map.controls[google.maps.ControlPosition.BOTTOM].clear(loader);
+
         },
         error: function (jqXHR) {
             data = jQuery.parseJSON(jqXHR.responseText);
@@ -184,40 +216,6 @@ $("document").ready(function () {
 
 });
 
-
-function toggleHeatmap() {
-    heatmap.setMap(heatmap.getMap() ? null : map);
-}
-
-function changeGradient() {
-    var gradient = [
-        'rgba(0, 255, 255, 0)',
-        'rgba(0, 255, 255, 1)',
-        'rgba(0, 191, 255, 1)',
-        'rgba(0, 127, 255, 1)',
-        'rgba(0, 63, 255, 1)',
-        'rgba(0, 0, 255, 1)',
-        'rgba(0, 0, 223, 1)',
-        'rgba(0, 0, 191, 1)',
-        'rgba(0, 0, 159, 1)',
-        'rgba(0, 0, 127, 1)',
-        'rgba(63, 0, 91, 1)',
-        'rgba(127, 0, 63, 1)',
-        'rgba(191, 0, 31, 1)',
-        'rgba(255, 102, 0, 1)'
-    ];
-    heatmap.setOptions({
-        gradient: heatmap.get('gradient') ? null : gradient
-    });
-}
-
-function changeRadius() {
-    heatmap.setOptions({radius: heatmap.get('radius') ? null : 20});
-}
-
-function changeOpacity() {
-    heatmap.setOptions({opacity: heatmap.get('opacity') ? null : 0.2});
-}
 function createRoute() {
     $.each(routeArray, function (route, valR) {
         var flightPath = new google.maps.Polyline({
@@ -261,58 +259,4 @@ function displayRoute() {
         }
         heatmap.setMap(map);
     }
-}
-
-function routeToStaticMapURL(route) {
-
-    var centerLat = 54.988744;
-    var centerLng = 73.369271;
-    var maxLat = (route[0]).lat();
-    var minLat = (route[0]).lat();
-    var maxLng = (route[0]).lng();
-    var minLng = (route[0]).lng();
-    centerLat = 0;
-    centerLng = 0;
-    for(i = 0; i < (route).length; i++) {
-        centerLat += (route[i]).lat();
-        centerLng += (route[i]).lng();
-        if((route[i]).lat() > maxLat) {
-            maxLat = (route[i]).lat();
-        }
-        if((route[i]).lat() < minLat) {
-            minLat = (route[i]).lat();
-        }
-        if((route[i]).lng() > maxLng) {
-            maxLng = (route[i]).lng();
-        }
-        if((route[i]).lng() < minLng) {
-            minLng = (route[i]).lng();
-        }
-    }
-    centerLat /= route.length;
-    centerLng /= route.length;
-
-    var mapWidth = 300;
-    var mapHeight = 300;
-    var worldCoordinateLength = (maxLat - minLat)*(maxLat - minLat) + (maxLng - minLng)*(maxLng - minLng);
-    var pixselCoordinateLength = mapWidth*mapWidth + mapHeight*mapHeight;
-    var zoom = Math.log(pixselCoordinateLength/worldCoordinateLength) / (2*Math.log(2));
-    zoom -= zoom%1;
-
-    var mapType = "roadmap";
-    var mapSensor = "false";
-    var urlImg = "http://maps.googleapis.com/maps/api/staticmap";
-    urlImg += "?center=" + centerLat + "," + centerLng;
-    urlImg += "&zoom=" + zoom;
-    urlImg += "&size=" + mapWidth + "x" + mapHeight;
-    urlImg += "&maptype=" + mapType;
-    urlImg += "&sensor=" + mapSensor;
-
-    urlImg += "&path=color:0x0000ff|weight:5"
-    for(i = 0; i < route.length; ) {
-        var step = Math.floor( route.length/10 );
-        urlImg += "|" + (route[i]).lat() + "," + (route[i]).lng();
-        i += step;
-    }
-    return urlImg;
 }
