9becd05ac289f9227746d11b78534cbc8fdf375d
Filter94 <IamFilter94@gmail.com>
2014-08-06 12:23:55 +0700

    Bit of code refactoring and bug with multiple loader now fixedg

--- web-app/js/login.js
+++ web-app/js/login.js
@@ -10,10 +10,11 @@ $('document').ready(function () {
     document.getElementById('close_log').onclick = function () {
         document.getElementById('parent_log').style.display = 'none';
     }
-    document.getElementById('p_log').onclick = function () {
-        document.getElementById('parent_log').style.display = 'block';
+    if( document.getElementById('p_log') != null){
+        document.getElementById('p_log').onclick = function () {
+            document.getElementById('parent_log').style.display = 'block';
+        }
     }
-
     $('#loginForm').submit(function (e) {
 
     });
