4f5a90dbbbb0c0dcd7189598d5bb1c8fe1e9ae9d
Filter94 <IamFilter94@gmail.com>
2014-08-15 12:57:00 +0700

    Bootstrap develop env fix

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -12,7 +12,7 @@ class BootStrap {
                 def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
                 def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
 
-                def testUser = new User(username: 'me', enabled: true, password: 'password')
+                def testUser = new User(username: 'me', enabled: true, password: 'password', uid: "")
                 testUser.save(flush: true)
 
                 UserRole.create testUser, adminRole, true
@@ -22,7 +22,7 @@ class BootStrap {
                 def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
                 def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
 
-                def testUser = new User(username: 'me', enabled: true, password: 'password')
+                def testUser = new User(username: 'me', enabled: true, password: 'password', uid: "")
                 testUser.save(flush: true)
 
                 UserRole.create testUser, adminRole, true
