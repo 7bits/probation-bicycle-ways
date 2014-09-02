400241e11da35d1aa3c9e708e3f3c712c4de2862
Filter94 <IamFilter94@gmail.com>
2014-09-02 14:47:16 +0700

    Fixed name generation.

--- grails-app/controllers/likebike/VkController.groovy
+++ grails-app/controllers/likebike/VkController.groovy
@@ -2,19 +2,33 @@ package likebike
 
 class VkController {
 
+    static final int MAX_NUMBER_OF_IVANS = 100;
+
     def springSecurityService
 
     def auth() {
-        def params = params
         String uid = params['uid']
-        String secret_key = "S4Cy4PaFEw9X1rZ6nukk";
-        String API_ID = "4479951"
+        String secret_key = "YIe1xEs8vWkImchof0Ll";
+        String API_ID = "4512659"
         def hash = params['hash']
         def ourHash = (API_ID + uid + secret_key).encodeAsMD5()
         if (hash == ourHash) {
             def user = likebike.User.findByUid(uid)
             if (!user) {
-                user = new likebike.User(username: params['first_name'] + " " + params['last_name'],
+                def username = params['first_name'] + " " + params['last_name']
+                if(likebike.User.findByUsername(username)) {
+                    int i;
+                    for (i = 1; i < MAX_NUMBER_OF_IVANS; i++) {
+                        if (!likebike.User.findByUsername(username + i)) {
+                            username += i
+                            break
+                        }
+                    }
+                    if(i == MAX_NUMBER_OF_IVANS){
+                        render "У нас и так хватает людей с именем, таким же, как ваше. Убирайтесь или, либо смените имя."
+                    }
+                }
+                user = new likebike.User(username: username,
                         uid: uid,
                         enabled: true,
                         password: hash
