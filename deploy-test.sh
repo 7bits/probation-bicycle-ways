19dbb28b2772ce05924f83a4470c6f98e7e7a172
Filter94 <iamFilter94@gmail.com>
2014-10-11 12:19:59 +0700

    Changed deployment to test server

--- deploy-test.sh
+++ deploy-test.sh
@@ -5,6 +5,6 @@ grails war
 #sync assets to server
 rsync -r -e 'ssh -p 23722' web-app/* root@office.7bits.it:/home/velo-user/assets/
 #copy war to remote server
-scp target/LikeBike-0.2.war root@192.168.1.237:/var/lib/tomcat7/webapps/ROOT.war
+scp -P 23722 target/LikeBike-0.2.war  root@office.7bits.it:/var/lib/tomcat7/webapps/ROOT.war
 #restart tomcat on server
 ssh -p 23722 root@office.7bits.it 'service tomcat7 restart'
